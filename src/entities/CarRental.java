a71d897ea71620d43fa6a6f27b6df54c55c881cf
CarinhaDaEsquina7234 <rafaeloliveirams7234@gmail.com>
2024-09-20 20:50:02 -0300

    Initial Commit

---
+++ src/entities/CarRental.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.Date;
+
+public class CarRental {
+
+	private Date start;
+	private Date finish;
+	
+	private Vehicle vehicle;
+	private Invoice Invoice;
+	
+	public CarRental()
+	{
+		
+	}
+
+	public CarRental(Date start, Date finish, Vehicle vehicle, entities.Invoice invoice) {
+		super();
+		this.start = start;
+		this.finish = finish;
+		this.vehicle = vehicle;
+		Invoice = invoice;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getFinish() {
+		return finish;
+	}
+
+	public void setFinish(Date finish) {
+		this.finish = finish;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public Invoice getInvoice() {
+		return Invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		Invoice = invoice;
+	}
+	
+	
+	
+}
